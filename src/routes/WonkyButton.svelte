b3d7d3b4c66297f11a6b0d67b990ff02314283b8
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-19 08:59:27 +0800

    feat: add WonkyButton from last assignment

--- src/routes/WonkyButton.svelte
+++ src/routes/WonkyButton.svelte
@@ -1,2 +1,19 @@
-<!-- Replace this file with your submission to the previous assignment -->
-<p>Hello world!</p>
+<script>
+	export let value = 0;
+
+	function increment() {
+		value += Math.floor(Math.random() * 10);
+	}
+</script>
+
+<button on:click={increment}>
+	{#if value % 15 == 0}
+		FizzBuzz {value}
+	{:else if value % 3 == 0}
+		Fizz {value}
+	{:else if value % 5 == 0}
+		Buzz {value}
+	{:else}
+		{value}
+	{/if}
+</button>
